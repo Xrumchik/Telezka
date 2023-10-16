0ad5da4854a45ffb716e130f7249445c70eecfdb
Xrumchik <110850654+Xrumchik@users.noreply.github.com>
2023-10-16 23:26:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,43 @@ CORS(app)
 
 
 def openpost():
-    return "heeey"
+    return "go"
 
 
-@app.route('/process_json', methods=['GET','POST'])
-def process_json():
+@app.route('/straight', methods=['GET','POST'])
+def straight():
     if request.method == 'POST':
-        print(1)
-        return openpost()
+        print('go')
+        return 'go'
+
+@app.route('/back', methods=['GET','POST'])
+def back():
+    if request.method == 'POST':
+        print('back')
+        return 'back'
+
+@app.route('/left', methods=['GET','POST'])
+def left():
+    if request.method == 'POST':
+        print('left')
+        return 'left'
+
+@app.route('/right', methods=['GET','POST'])
+def right():
+    if request.method == 'POST':
+        print('right')
+        return 'right'
+
+@app.route('/stop', methods=['GET','POST'])
+def stop():
+    if request.method == 'POST':
+        print('stop')
+        return 'stop'
 
 
 
 
 if __name__=='__main__':
     app.run(host="192.168.1.7", port=4343)
+
+# curl -X POST -d "data=Hello" http://192.168.1.7:4343/
